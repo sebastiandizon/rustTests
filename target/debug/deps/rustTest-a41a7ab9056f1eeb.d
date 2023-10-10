b0390009d53ae65f71b5b4f0c65fd3361c2460b6
sebastiandizon <sebastiantingdizon@gmail.com>
2023-10-10 15:59:19 -0700

    Initial Commit

---
+++ target/debug/deps/rustTest-a41a7ab9056f1eeb.d
@@ -0,0 +1,5 @@
+/Users/sebastiandizon/rusttest/rustTest/target/debug/deps/rustTest-a41a7ab9056f1eeb: src/main.rs
+
+/Users/sebastiandizon/rusttest/rustTest/target/debug/deps/rustTest-a41a7ab9056f1eeb.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rustTest.d
@@ -0,0 +1,1 @@
+/Users/sebastiandizon/rusttest/rustTest/target/debug/rustTest: /Users/sebastiandizon/rusttest/rustTest/src/main.rs
